d3a3187b3f125f75f589b6fca740f9847cdb7086
Nikita Timonin <nikita@letsopen.co>
2023-10-09 17:55:21 +0300

    Initial Commit

---
+++ AppleMusicButtonTest2/AppleMusicButtonTest2App.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMusicButtonTest2App.swift
+//  AppleMusicButtonTest2
+//
+//  Created by Nikita Timonin on 09.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct AppleMusicButtonTest2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
